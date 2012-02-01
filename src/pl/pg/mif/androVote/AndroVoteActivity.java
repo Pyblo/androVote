85b036ffa168e3ace918034266a0a69958a35c6f
Lukas Redynk <mr.erdk@gmail.com>
2012-02-01 18:16:26 +0100

    Zmiany dokonane przez Joannę Matyjas.

--- src/pl/pg/mif/androVote/AndroVoteActivity.java
+++ src/pl/pg/mif/androVote/AndroVoteActivity.java
@@ -1,37 +1,81 @@
 package pl.pg.mif.androVote;
 
-import java.io.IOException;
-
-import pl.pg.mif.androVote.WCFConnector.BadLoginException;
+import pl.pg.mif.androVote.WCFConnector.UserInfo;
 import pl.pg.mif.androVote.WCFConnector.WCFConnector;
-
-import pl.pg.mif.androVote.R;
-
+import pl.pg.mif.androVote.WCFConnector.Exceptions.BadLoginException;
+import pl.pg.mif.androVote.WCFConnector.Exceptions.IOSettingsException;
+import pl.pg.mif.androVote.WCFConnector.Exceptions.ServerConnectionException;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class AndroVoteActivity extends Activity {
 
+	private Button zaloguj;
+	private Button zamknij;
+	private Button ustawienia;
+	private EditText login;
+	private EditText haslo;
+	private String logintxt;
+	private String haslotxt;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        TextView tv = (TextView) findViewById(R.id.main_test_textview);
-        WCFConnector wcf = new WCFConnector("http://10.0.2.2:23323/RW_GLOSOWANIE_WcfDataService.svc/");
-        try {
-			tv.setText(wcf.Login("kak", "kaka"));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (BadLoginException e) {
-			// TODO Auto-generated catch block
-			tv.setText(e.message);
-			e.printStackTrace();
-		}
+        
+		this.zamknij = (Button) this.findViewById(R.id.button3);
+		this.zamknij.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				finish();
+			}
+		});
+		this.zaloguj = (Button) this.findViewById(R.id.button1);
+		this.zaloguj.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+				login = (EditText) findViewById(R.id.editText1);
+				logintxt = login.getText().toString();
+				haslo = (EditText) findViewById(R.id.editText2);
+				haslotxt = haslo.getText().toString();
+
+			}
+		});
+		this.ustawienia = (Button) this.findViewById(R.id.button2);
+		this.ustawienia.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+        
+        WCFConnector wcf = null; 
+        try{
+        	wcf = new WCFConnector(this);
+        } catch (IOSettingsException e)
+        {
+        	
+        }
+        
+        if (wcf != null)
+        {
+	        try {
+	        	wcf.Login(logintxt, haslotxt);
+			} catch (ServerConnectionException e) {
+				e.printStackTrace();
+			} catch (BadLoginException e) {
+				e.printStackTrace();
+			}
+        } else {
+        }
     }
     
     @Override
